8c4499e82cebc00ef424ad6bd56a4ce302b0b257
PoterPan <poterpan5466@gmail.com>
2022-03-26 16:05:05 +0800

    Initial Commit

---
+++ MyRefrigerator/MyRefrigeratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRefrigeratorApp.swift
+//  MyRefrigerator
+//
+//  Created by Poter Pan on 2022/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MyRefrigeratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
